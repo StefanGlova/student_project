6a40bf89169e73df8b6d7b96c82e4df6f4a02081
StefanGlova <glovastefan@protonmail.com>
2023-08-24 22:46:33 +0100

    Student model update, first migration crated

--- students/models.py
+++ students/models.py
@@ -9,3 +9,5 @@ class Student(models.Model):
     field_of_study = models.CharField(max_length=50)
     gpa = models.FloatField()
 
+    def __str__(self):
+        return f"student: {self.first_name} {self.last_name}"
